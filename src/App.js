6209c3300b5af12d7dda0449156f7e6dca950863
Caio Abdon <caio.alves54@etec.sp.gov.br>
2023-04-12 22:27:52 -0300

    Criando contexto de autenticação

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import './App.css';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { onAuthStateChanged } from 'firebase/auth';
+
+import { useState, useEffect } from 'react';
+import { useAuthentication } from './hooks/useAuthentication';
+
+// context
+import { AuthContextProvider } from './context/AuthContext';
+
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Navbar from './components/Navbar';
@@ -8,20 +16,39 @@ import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 
 function App() {
+
+  const [user, setUser] = useState(undefined);
+  const { auth } = useAuthentication();
+
+  const loadingUser = user === undefined
+
+  if(loadingUser) {
+    return <p>Carregando...</p>;
+  }
+
+  useEffect(() => {
+
+    onAuthStateChanged(auth, (user) => {
+      setUser(user)
+    })
+  }, [auth]);
+
   return (
     <div className="App">
-      <BrowserRouter>
-      <Navbar />
-        <div className="container">
-          <Routes>
-              <Route path='/' element={ <Home /> }/>
-              <Route path='/about' element={ <About /> }/>
-              <Route path='/login' element={ <Login /> }/>
-              <Route path='/register' element={ <Register /> }/>
-          </Routes>
-        </div>
-      <Footer />
-      </BrowserRouter>
+      <AuthContextProvider value={{ user }}>
+        <BrowserRouter>
+        <Navbar />
+          <div className="container">
+            <Routes>
+                <Route path='/' element={ <Home /> }/>
+                <Route path='/about' element={ <About /> }/>
+                <Route path='/login' element={ <Login /> }/>
+                <Route path='/register' element={ <Register /> }/>
+            </Routes>
+          </div>
+        <Footer />
+        </BrowserRouter>
+      </AuthContextProvider>
     </div>
   );
 }
